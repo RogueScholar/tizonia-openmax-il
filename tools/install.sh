931f939ec2f906c8449d2acdb7ec81c9ff1e8143
Restyled.io <commits@restyled.io>
2020-03-18 13:15:48 +0000

    Restyled by shfmt

--- tools/install.sh
+++ tools/install.sh
@@ -31,145 +31,173 @@
 
 # Make sure the installation happens safely
 if [[ "$(id -u)" -eq 0 ]]; then
-    if [[ ( -z "$1" ) || ( "$1" != "--safe" ) ]]; then
-        echo "WARNING: Running as root; installation aborted."
-        echo "Please add '--safe' to bypass this check and continue installing as root."
-        exit 1
-    else
-        echo "WARNING: Running as root with --safe option; installation continues..."
-    fi
+  if [[ (-z "$1") || ("$1" != "--safe") ]]; then
+    echo "WARNING: Running as root; installation aborted."
+    echo "Please add '--safe' to bypass this check and continue installing as root."
+    exit 1
+  else
+    echo "WARNING: Running as root with --safe option; installation continues..."
+  fi
 fi
 
 # Run the install actions in a subshell
 (
-RELIDS=$(cat /etc/*-release)
-if echo "$RELIDS" | grep raspbian; then
+  RELIDS=$(cat /etc/*-release)
+  if echo "$RELIDS" | grep raspbian; then
     DISTRO="raspbian"
     if echo "$RELIDS" | grep buster; then
-        RELEASE="buster" ; PYVER=3 ; MOPRELEASE="buster"
+      RELEASE="buster"
+      PYVER=3
+      MOPRELEASE="buster"
     elif echo "$RELIDS" | grep stretch; then
-        RELEASE="stretch"; PYVER=2 ; MOPRELEASE="stretch"
+      RELEASE="stretch"
+      PYVER=2
+      MOPRELEASE="stretch"
     elif echo "$RELIDS" | grep jessie; then
-        RELEASE="jessie" ; PYVER=2 ; MOPRELEASE="jessie"
+      RELEASE="jessie"
+      PYVER=2
+      MOPRELEASE="jessie"
     else
-        echo "Can't find a supported Raspbian distribution."
-        exit 1
+      echo "Can't find a supported Raspbian distribution."
+      exit 1
     fi
-elif echo "$RELIDS" | grep stretch; then
-  DISTRO="debian" ; RELEASE="stretch" ; PYVER=2 ; MOPRELEASE="stretch"
-elif echo "$RELIDS" | grep -E 'buster'; then
-  DISTRO="debian" ; RELEASE="buster" ; PYVER=3 ; MOPRELEASE="buster"
-elif echo "$RELIDS" | grep -E 'bullseye|kali-rolling'; then
-  # NOTE: Kali Linux is based on Debian Testing, which is currently codenamed 'Bullseye'
-  DISTRO="debian" ; RELEASE="bullseye" ; PYVER=3 ; MOPRELEASE="buster"
-elif echo "$RELIDS" | grep -E 'trusty|freya|qiana|rebecca|rafaela|rosa'; then
-  # NOTE: Elementary OS 'freya' is based on trusty
-  # NOTE: LinuxMint 'qiana' 'rebecca' 'rafaela' 'rosa' are all based on trusty
-  DISTRO="ubuntu" ; RELEASE="trusty" ; PYVER=2 ; MOPRELEASE="jessie"
-elif echo "$RELIDS" | grep vivid; then
-  DISTRO="ubuntu" ; RELEASE="vivid" ; PYVER=2 ; MOPRELEASE="jessie"
-elif echo "$RELIDS" | grep -E 'xenial|loki|sarah|serena|sonya|sylvia'; then
-  # NOTE: Elementary OS 'loki' is based on xenial
-  # NOTE: Linux Mint 'sarah', 'serena', 'sonya' and 'sylvia' are based on xenial
-  DISTRO="ubuntu" ; RELEASE="xenial" ; PYVER=2 ; MOPRELEASE="jessie"
-elif echo "$RELIDS" | grep -E 'bionic|juno|hera|tara|tessa|tina|tricia'; then
-  # NOTE: Elementary OS 'juno', and 'hera' are based on bionic
-  # NOTE: Linux Mint 'tara', 'tessa', 'tina', and 'tricia' are based on bionic
-  # NOTE: Most of the time, binaries compiled on 18.04 will work on newer
-  # releases, meaning you can try adding newer releases to the bionic conditional
-  # (e.g. 'disco|bionic|juno|...'), to support installation on a newer system;
-  # however, do this 'at your own risk', as not all features will be guaranteed to work.
-  DISTRO="ubuntu" ; RELEASE="bionic" ; PYVER=3 ; MOPRELEASE="stretch"
-else
-  echo "Can't find a supported Debian or Ubuntu-based distribution."
-  exit 1
-fi
+  elif echo "$RELIDS" | grep stretch; then
+    DISTRO="debian"
+    RELEASE="stretch"
+    PYVER=2
+    MOPRELEASE="stretch"
+  elif echo "$RELIDS" | grep -E 'buster'; then
+    DISTRO="debian"
+    RELEASE="buster"
+    PYVER=3
+    MOPRELEASE="buster"
+  elif echo "$RELIDS" | grep -E 'bullseye|kali-rolling'; then
+    # NOTE: Kali Linux is based on Debian Testing, which is currently codenamed 'Bullseye'
+    DISTRO="debian"
+    RELEASE="bullseye"
+    PYVER=3
+    MOPRELEASE="buster"
+  elif echo "$RELIDS" | grep -E 'trusty|freya|qiana|rebecca|rafaela|rosa'; then
+    # NOTE: Elementary OS 'freya' is based on trusty
+    # NOTE: LinuxMint 'qiana' 'rebecca' 'rafaela' 'rosa' are all based on trusty
+    DISTRO="ubuntu"
+    RELEASE="trusty"
+    PYVER=2
+    MOPRELEASE="jessie"
+  elif echo "$RELIDS" | grep vivid; then
+    DISTRO="ubuntu"
+    RELEASE="vivid"
+    PYVER=2
+    MOPRELEASE="jessie"
+  elif echo "$RELIDS" | grep -E 'xenial|loki|sarah|serena|sonya|sylvia'; then
+    # NOTE: Elementary OS 'loki' is based on xenial
+    # NOTE: Linux Mint 'sarah', 'serena', 'sonya' and 'sylvia' are based on xenial
+    DISTRO="ubuntu"
+    RELEASE="xenial"
+    PYVER=2
+    MOPRELEASE="jessie"
+  elif echo "$RELIDS" | grep -E 'bionic|juno|hera|tara|tessa|tina|tricia'; then
+    # NOTE: Elementary OS 'juno', and 'hera' are based on bionic
+    # NOTE: Linux Mint 'tara', 'tessa', 'tina', and 'tricia' are based on bionic
+    # NOTE: Most of the time, binaries compiled on 18.04 will work on newer
+    # releases, meaning you can try adding newer releases to the bionic conditional
+    # (e.g. 'disco|bionic|juno|...'), to support installation on a newer system;
+    # however, do this 'at your own risk', as not all features will be guaranteed to work.
+    DISTRO="ubuntu"
+    RELEASE="bionic"
+    PYVER=3
+    MOPRELEASE="stretch"
+  else
+    echo "Can't find a supported Debian or Ubuntu-based distribution."
+    exit 1
+  fi
 
-# Let's make sure these packages are already installed before trying to install
-# anything else.  Some of these packages are needed to make sure that Tizonia's
-# Python dependencies are correctly installed from PIP.
-sudo apt-get -y --force-yes install build-essential git curl apt-transport-https libffi-dev libssl-dev libxml2-dev libxslt1-dev
-if [[ "$?" -ne 0 ]]; then
+  # Let's make sure these packages are already installed before trying to install
+  # anything else.  Some of these packages are needed to make sure that Tizonia's
+  # Python dependencies are correctly installed from PIP.
+  sudo apt-get -y --force-yes install build-essential git curl apt-transport-https libffi-dev libssl-dev libxml2-dev libxslt1-dev
+  if [[ "$?" -ne 0 ]]; then
     echo "Oops. Some important dependencies failed to install!."
     echo "Please re-run the install script."
     exit 1
-fi
+  fi
 
-# Add Mopidy's APT archive
-if [[ ! -f /etc/apt/sources.list.d/mopidy.list ]]; then
+  # Add Mopidy's APT archive
+  if [[ ! -f /etc/apt/sources.list.d/mopidy.list ]]; then
     echo "Setting up Mopidy's APT archive for $DISTRO:$MOPRELEASE (to install 'libspotify')"
     curl 'https://apt.mopidy.com/mopidy.gpg' | sudo apt-key add -
     sudo curl https://apt.mopidy.com/"$MOPRELEASE".list -o /etc/apt/sources.list.d/mopidy.list
-fi
+  fi
 
-# Add Tizonia's APT archive
-grep -q "dl.bintray.com/tizonia" /etc/apt/sources.list
-if [[ "$?" -eq 1 ]]; then
+  # Add Tizonia's APT archive
+  grep -q "dl.bintray.com/tizonia" /etc/apt/sources.list
+  if [[ "$?" -eq 1 ]]; then
     echo "Setting up Tizonia's Bintray APT archive for $DISTRO:$RELEASE"
     curl -k 'https://bintray.com/user/downloadSubjectPublicKey?username=tizonia' | sudo apt-key add -
     echo "deb https://dl.bintray.com/tizonia/$DISTRO $RELEASE main" | sudo tee -a /etc/apt/sources.list
-fi
+  fi
 
-# Resynchronize APT's package index files
-sudo apt-get update
+  # Resynchronize APT's package index files
+  sudo apt-get update
 
-# Python dependencies (NOTE: using pip2 or pip3 depending on which Python
-# version is required for the version of Tizonia being installed.
-if [[ PYVER=3 ]]; then
-    sudo apt-get -y remove python3-pip \
-        && sudo apt-get -y install python3-dev python3-pip \
-        && sudo -H pip3 install --upgrade \
-                gmusicapi \
-                soundcloud \
-                youtube-dl \
-                pafy \
-                pycountry \
-                titlecase \
-                pychromecast \
-                plexapi \
-                fuzzywuzzy \
-                eventlet \
-                python-Levenshtein \
-                joblib \
-        && sudo -H pip3 install git+https://github.com/plamere/spotipy.git --upgrade
-else
-    sudo apt-get -y install python-dev python-pip \
-        && sudo -H pip2 install --upgrade \
-                gmusicapi \
-                soundcloud \
-                youtube-dl \
-                pafy \
-                pycountry \
-                titlecase \
-                pychromecast \
-                plexapi \
-                fuzzywuzzy \
-                eventlet \
-                python-Levenshtein \
-        && sudo -H pip2 install git+https://github.com/plamere/spotipy.git --upgrade
-fi
+  # Python dependencies (NOTE: using pip2 or pip3 depending on which Python
+  # version is required for the version of Tizonia being installed.
+  if [[ PYVER=3 ]]; then
+    sudo apt-get -y remove python3-pip &&
+      sudo apt-get -y install python3-dev python3-pip &&
+      sudo -H pip3 install --upgrade \
+        gmusicapi \
+        soundcloud \
+        youtube-dl \
+        pafy \
+        pycountry \
+        titlecase \
+        pychromecast \
+        plexapi \
+        fuzzywuzzy \
+        eventlet \
+        python-Levenshtein \
+        joblib &&
+      sudo -H pip3 install git+https://github.com/plamere/spotipy.git --upgrade
+  else
+    sudo apt-get -y install python-dev python-pip &&
+      sudo -H pip2 install --upgrade \
+        gmusicapi \
+        soundcloud \
+        youtube-dl \
+        pafy \
+        pycountry \
+        titlecase \
+        pychromecast \
+        plexapi \
+        fuzzywuzzy \
+        eventlet \
+        python-Levenshtein &&
+      sudo -H pip2 install git+https://github.com/plamere/spotipy.git --upgrade
+  fi
 
-
-# Install 'libspotify'
-if [[ "$?" -eq 0 ]]; then
+  # Install 'libspotify'
+  if [[ "$?" -eq 0 ]]; then
     sudo apt-get -y install libspotify12
-fi
+  fi
 
-# Install Tizonia
-if [[ "$?" -eq 0 ]]; then
+  # Install Tizonia
+  if [[ "$?" -eq 0 ]]; then
     sudo apt-get -y install tizonia-all
-fi
+  fi
 
-# Simple test to verify that everything went well
-TIZ_CONFIG_FILE="$HOME/.config/tizonia/tizonia.conf"
-which tizonia > /dev/null
-if [[ "$?" -eq 0 ]]; then
-    echo ; tizonia ; echo
+  # Simple test to verify that everything went well
+  TIZ_CONFIG_FILE="$HOME/.config/tizonia/tizonia.conf"
+  which tizonia >/dev/null
+  if [[ "$?" -eq 0 ]]; then
+    echo
+    tizonia
+    echo
     printf "Tizonia is now installed.\n\n"
     printf "Please add Spotify, Google Music, Soundcloud, and Plex credentials to : $TIZ_CONFIG_FILE\n"
-else
+  else
     echo "Oops. Something went wrong!"
     exit 1
-fi
+  fi
 
 ) # end running in a subshell
